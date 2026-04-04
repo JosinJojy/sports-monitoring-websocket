b30d94b89b3ff1ec99e5c3e8acab9f2046754c2e
JosinJojy <josinjojy1@gmail.com>
2026-04-04 19:38:59 +0530

    Implemented arcjet security

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { matchRouter } from './routes/matches.js'
 import http from 'http'
 import { attachWebSocketServer } from './ws/server.js'
+import { securityMiddleware } from './arcjet.js'
 
 
 const port = Number(process.env.PORT || 8000)
@@ -15,6 +16,8 @@ app.use(express.json())
 app.get('/',(req, res)=>{
     res.send("Hello from express server")
 })
+
+app.use(securityMiddleware())
 
 app.use('/matches', matchRouter)
 
